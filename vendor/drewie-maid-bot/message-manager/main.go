c572bc4f670db75267ba97f2414ecf435806d427
Drew Dru <drew.drux@gmail.com>
2019-03-19 22:43:11 +0700

    heroku add local package

---
+++ vendor/drewie-maid-bot/message-manager/main.go
@@ -0,0 +1,48 @@
+package messagemanager
+
+import (
+	"log"
+	"regexp"
+	"strings"
+
+	tgbotApi "github.com/Syfaro/telegram-bot-api"
+)
+
+type MessageManager struct {
+	Bot    *tgbotApi.BotAPI `tgbotApi.BotAPI:"telegram bot api"`
+	Update *tgbotApi.Update `tgbotApi.Update:"update response"`
+}
+
+func (manager *MessageManager) Process() error {
+	log.Printf("From: %+v;%+v;%+v. Text: %+v\n",
+		manager.Update.Message.From.ID,
+		manager.Update.Message.From.LanguageCode,
+		manager.Update.Message.From,
+		manager.Update.Message.Text)
+
+	data := []string{"", manager.Update.Message.Text}
+	if manager.Update.Message.Text[0] == '/' {
+		regSpace := regexp.MustCompile(" ")
+		data = regSpace.Split(data[1], 2)
+	}
+
+	response := ""
+	switch data[0] {
+	case "":
+		if strings.Contains(data[1], "?") {
+			response = "42"
+		} else {
+			response = "Not a command: " + data[1]
+		}
+	case "/help":
+		response = "/help - for help\n"
+	default:
+		response = "Command: " + data[0] + "; data:" + data[1]
+	}
+
+	msg := tgbotApi.NewMessage(manager.Update.Message.Chat.ID,
+		response)
+	msg.ReplyToMessageID = manager.Update.Message.MessageID
+	manager.Bot.Send(msg)
+	return nil
+}
